767cb32b810ceb962d13f1549619bfe0c23265a7
DAVID OCHIENO <ochienooduory@outlook.com>
2026-01-23 22:27:37 +0300

    Mobile Test V1

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LawAfrica.API.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { ok = true, service = "LawAfrica.API", utc = DateTime.UtcNow });
+    }
+}
