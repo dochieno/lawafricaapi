2ceb9b65be2081110c123603b18b4db0b24bbfdc
DAVID OCHIENO <ochienooduory@outlook.com>
2026-02-22 18:23:37 +0300

    Find Layer V004

---
+++ Migrations/20260222152234_AddLawyerServicesAndRateCardsV0.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LawAfrica.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLawyerServicesAndRateCardsV0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillingUnit",
+                table: "LawyerServiceOfferings",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingUnit",
+                table: "LawyerServiceOfferings");
+        }
+    }
+}
